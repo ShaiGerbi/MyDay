a1255fab5d0e3ba7a1571c0f3ea935cb5e7d019e
Shai Gerbi <ShaiGerbi@gmail.com>
2020-12-19 21:19:34 +0200

    Add tasks to the router

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,3 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::view('/home', 'home')->name('home');
+
+Route::resource('tasks', TaskController::class);
