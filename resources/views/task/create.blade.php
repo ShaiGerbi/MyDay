1bac48127c522d71cc303485ed1ac2d298b55b6b
Shai Gerbi <ShaiGerbi@gmail.com>
2020-12-29 20:55:29 +0200

    Add vuetify elements to the tasks pages

--- resources/views/task/create.blade.php
+++ resources/views/task/create.blade.php
@@ -2,6 +2,43 @@
 
 @section('content')
     <v-container>
-        
+        <v-form method="post" action="{{ route('tasks.store') }}">
+
+            @csrf
+
+            <v-text-field label="Task Name" name="name" value="{{ old('name') }}" required autofocus></v-text-field>
+
+
+            <v-row>
+                <v-col cols="12" md="6">
+                    <v-select label="Category" :items="categories"></v-select>
+                </v-col>
+
+                <v-col cols="12" md="6">
+                    <v-select label="Color" :items="colors"></v-select>
+                </v-col>
+            </v-row>
+
+
+            <v-row>
+                <v-col cols="12" md="6">
+                    <v-select label="Equipment" :items="equipments"></v-select>
+                </v-col>
+
+                <v-col cols="12" md="6">
+                    <v-select label="Assign" :items="assigns"></v-select>
+                </v-col>
+            </v-row>
+
+
+            <v-select label="Priority" :items="priorities"></v-select>
+
+            <v-textarea label="Notes" rows="3"></v-textarea>
+
+
+            <v-btn type="submit">submit</v-btn>
+
+
+        </v-form>
     </v-container>
 @endsection
--- resources/views/task/index.blade.php
+++ resources/views/task/index.blade.php
@@ -3,5 +3,26 @@
 @section('content')
     <v-container>
 
+            <v-card>
+                <v-list>
+                    <v-list-item-group>
+
+                        @foreach ($tasks as $task)
+
+                            <v-list-item href="{{ route('tasks.edit', $task) }}">
+                                <v-list-item-action>
+                                    <v-checkbox></v-checkbox>
+                                </v-list-item-action>
+                                <v-list-item-content>
+                                    <v-list-item-title>{{ $task->name }}</v-list-item-title>
+                                </v-list-item-content>
+                            </v-list-item>
+
+                        @endforeach
+
+                    </v-list-item-group>
+                </v-list>
+            </v-card>
+
     </v-container>
 @endsection
